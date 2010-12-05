cb95aa7fbde17c3a25146d285cf7384a17d5cb11
Jared Williams <jaredwilli@gmail.com>
2010-12-04 21:41:14 -0500

    Full commit with all changes. New plugin options: removeHref, removeTitle, style, and offsetx, offsety (not working yet). Updated demo page and readme file. New version number 1.0.4

--- jquery.tipitup.js
+++ jquery.tipitup.js
@@ -1,7 +1,7 @@
 /**
- * Name: TipItUp v1.0.3 - Tipping it up...
- * Author: Jared Williams - http://new2wp.com
- * URI: http://new2jquery.com/plugins/tipitup-jquery-image-tooltip
+ * Name: TipItUp v1.0.4 - Tipping it up...
+ * Author: Jared Williams - http://new2jquery.com
+ * URI: http://new2jquery.com/jquery/tipitup-jquery-image-tooltip
  * Demo: http://www.jsfiddle.net/jaredwilli/gyVkB/7/
  * Copyright (c) 2010 jaredwilli
  * Licensed under the MIT License: http://www.opensource.org/licenses/mit-license.php
@@ -15,8 +15,8 @@
 			removeHref: true,
 			removeTitle: true,
 			offset: 10,
-			offsety: 10,
-			offsetx: 2,
+			offsety: 0,
+			offsetx: 0,
 			fadeIn: 200,
 			fadeOut: 200,
 			delay: 10,

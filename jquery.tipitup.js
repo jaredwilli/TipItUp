a8a036537e1c17fe1c7058b708ad3fda95c56f4f
Jared Williams <jaredwilli@gmail.com>
2010-12-04 20:44:08 -0500

    non-working offsetx and offsety. committing so i have a version that almost works to possibly use later

--- jquery.tipitup.js
+++ jquery.tipitup.js
@@ -8,85 +8,132 @@
  */
 
 (function($) {
-    $.fn.TipItUp = function(options) {
+	$.fn.TipItUp = function(options) {
 
-        var defaults = {
-            offset: 10,
-            speed: 200,
-            delay: 10,
-			style: {}
-        };
+		var defaults = {
+			title: '',
+			removeHref: true,
+			removeTitle: true,
+			offset: 10,
+			offsety: 50,
+			offsetx: 20,
+			fadeIn: 200,
+			fadeOut: 200,
+			delay: 10,
+			style: {
+				backgroundColor: '#CCC',
+				border: '1px solid #999',
+				color: '#333',
+				padding: '10px'
+			}
+		};
 
-        var settings = $.extend( {}, defaults, options );
-        
-        var $imgTip = $('<div class="tip"></div>').hide().prependTo('body'),
-            $win = $(window),
-            showTip;
+		var settings = $.extend( {}, defaults, options );
+		
+		var $imgTip = $('<div class="tip"></div>').hide().prependTo('body'),
+			$win = $(window),
+			showTip;
 
-        var tip = {
-            title: '',
-            href: '',
-            position: function(event) {
-                var positions = { x: event.pageX, y: event.pageY },
-                    dimensions = {
-                        x: [
-                            $win.width(),
-                            $imgTip.outerHeight()
-                        ],
-                        y: [
-                            $win.scrollTop() + $win.height(),
-                            $imgTip.outerHeight()
-                        ]
-                    };
-                
-                for (var axis in dimensions ) {
-                    if (dimensions[axis][0] < dimensions[axis][1] + positions[axis] + settings.offset) {
-                        positions[axis] -= dimensions[axis][1] - settings.offset;
-                    } else {
-                        positions[axis] += settings.offset;
-                    }
-                }
-                
-                $imgTip.css({ top: positions.y, left: positions.x });
-            }
-        };
-        
-        $(this).delegate('a', 'mouseover mouseout mousemove', function(event) {
-            
-            var link = this,
-                $link = $(this);
+		var tip = {
+			href: '',
+			title: settings.title,
+			position: function(event) {
+				var positions = { x: event.pageX, y: event.pageY },
+					dimensions = {
+						x: [
+							$win.width(),
+							$imgTip.outerHeight()
+						],
+						y: [
+							$win.scrollTop() + $win.height(),
+							$imgTip.outerHeight()
+						]
+					};
+				
+				for (var axis in dimensions ) {
+					if (dimensions[axis][0] < dimensions[axis][1] + positions[axis] + settings.offset) {
+						positions.y -= dimensions[axis][1] - settings.offsety;
+						positions.x -= dimensions[axis][1] - settings.offsetx;
+					} else {
+						positions.y += settings.offsety;
+						positions.x += settings.offsetx;
+					}
+				}
+				$imgTip.css({ 
+					top: positions.y,
+					left: positions.x,
+					'position': 'absolute', 
+					'zIndex': 10
+				});
+				$imgTip.css( settings.style );
+			}
+		};
+		
+		/* Create the tooltip and it's events */
+		$(this).delegate('a', 'mouseover mouseout mousemove', function(event) {
+			
+			var link = this,
+				$link = $(this);
 
-            if (event.type == 'mouseover') {
-                tip.link = link.href;
-                tip.title = link.title;
-                
-                showTip = setTimeout(function() {
-                    $link.data('active', true);
-                    tip.position(event);
+			/* If MouseOver */
+			if (event.type == 'mouseover') {
+				// set the href and title attributes
+				tip.link = link.href;
+				
+				// If settings.title is not set use the link title if it exists
+				if (settings.title == '') {
+					tip.title = link.title;
+				} else {
+					tip.title = settings.title;
+				}
+				
+				/* Show the tooltip */
+				showTip = setTimeout(function() {
+					$link.data('active', true);
+					tip.position(event);
 
-                    $imgTip.html('<img src="' + tip.link + '" alt="' + tip.title + '" /><br />' + tip.title)
-                    	.fadeOut( 'fast' ).fadeIn( settings.speed );
-	            // tip.link = link.href;
-        	    $link.attr('href', '');
-        	    $link.attr('title', '');
-                    
-                }, settings.delay );
-            }
+					$imgTip.html('<img src="' + tip.link + '" alt="' + tip.title + '" /><br />' + tip.title).fadeOut( settings.fadeOut ).fadeIn( settings.fadeIn );
+	
+					/**
+					 * Make the href and title of the link blank if set to true
+					 * in settings.removeHref and settings.removeTitle
+					 */
+					if (settings.removeHref) {
+						$link.attr('href', '');
+					}
+					if (settings.removeTitle) {
+						$link.attr('title', '');
+					}
 
-            if (event.type == 'mouseout') {
-                link.href = tip.link;
-                link.title = tip.title;
-                if ($link.data('active')) {
-                    $link.removeData('active');
-                    $imgTip.hide();
-                } else {
-                    clearTimeout(showTip);
-                }
-            }
-            
-            if (event.type == 'mousemove' && $link.data('active')) {
-                tip.position(event);    
-            }
-        });
-    };
+				}, settings.delay );
+			} // end MouseOver
+
+			/* If MouseOut */
+			if (event.type == 'mouseout') {
+				/**
+				 * If the href and title were removed
+				 * add them back to the link
+				 */
+				if (settings.removeHref) {
+					link.href = tip.link;
+				}
+				if (settings.removeTitle) {
+					link.title = tip.title;
+				}
+				
+				if ($link.data('active')) {
+					$link.removeData('active');
+					$imgTip.hide();
+				} else {
+					clearTimeout(showTip);
+				}
+			} // end MouseOut
+			
+			/* If MouseOver */
+			if (event.type == 'mousemove' && $link.data('active')) {
+				tip.position(event);	
+			} // end MouseOver
+			
+		});
+	};
 })(jQuery);
